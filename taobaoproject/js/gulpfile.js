5f7d64db8e33ea00fbcc365d861f039aba9012c6
ying <1012460852@qq.com>
2017-11-08 16:07:18 +0800

    初步提交

---
+++ taobaoproject/js/gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var connect= require('gulp-connect'); 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin'); 
